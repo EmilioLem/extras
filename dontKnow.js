003f017c53af924746b240cddf60f7f8f6583e92
emilemus <92602556+EmilioLem@users.noreply.github.com>
2022-12-19 21:52:09 -0600

    comeOnnnn

--- dontKnow.js
+++ dontKnow.js
@@ -7,3 +7,9 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 });
+
+//A function that returns a string
+
+var helloWorld = function() {
+    return 'Hello World';
+};
