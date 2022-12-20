58557653b8f33fd4c6ca346574bb915c19edca53
emilemus <92602556+EmilioLem@users.noreply.github.com>
2022-12-19 21:51:07 -0600

    ProbablyDontKnowCommitAndPush

---
+++ dontKnow.js
@@ -0,0 +1,9 @@
+//First we will console log a funny koke
+
+//An HTTP server to say Hello World
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
